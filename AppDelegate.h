240c4a8d675c592254a81be1d8e0267a9b0daae8
xuwening <aqua@aquadeMac-mini.local>
2014-09-02 12:03:24 +0800

    add test project

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextInputLimitTest
+//
+//  Created by aqua on 14-9-2.
+//  Copyright (c) 2014年 aqua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
